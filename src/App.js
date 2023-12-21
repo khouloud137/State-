ab896bb2b302f7247e46204b9d04133f2489abb5
khouloud137 <khouloudgouttai@gmail.com>
2023-12-21 14:26:58 +0100

    State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Persone from "./component/Persone";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Wael Al-Dahdouh",
+      bio: "JOURNALISTES AFRICAINS, LES NOUVEAUX RÉSISTANTS – Depuis le début de la guerre entre Israël et le Hamas, Wael Al-Dahdouh, journaliste vedette d'Al Jazeera et chef du bureau de la chaîne qatarie à Gaza, relate quotidiennement la violence inouïe infligée à la population palestinienne par l'État hébreu",
+      imgSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/9/93/Wael_Al_Dahdouh.png",
+      profession: "Journaliste",
+    },
+    shows: false,
+  };
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      shows: !prevState.shows,
+    }));
+  };
+  render() {
+    return (
+      <>
+        {this.state.shows && <Persone Person={this.state.Person} />}
+        <div className="btn">
+          <button onClick={this.toggleShow}>toggle me!</button>
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
